35881afa1a988d243d4dc41e090e489798c0004e
hyb-store <3063687049@qq.com>
2021-05-27 19:59:46 +0800

    后缀表达式

--- DataStructures/src/stack/PolandNotation.java
+++ DataStructures/src/stack/PolandNotation.java
@@ -2,6 +2,7 @@ package stack;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 后缀表达式   逆波兰表达式
@@ -18,10 +19,15 @@ public class PolandNotation {
     public static void main(String[] args) {
 
         //先定义逆波兰表达式
-        String suffixExpression = "3 4 + 5 × 6 -";
+        //String suffixExpression = "3 4 + 5 * 6 -";
+        //4 * 5 - 8 + 60 + 8 / 2 => 4 5 * 8 - 60 + 8 2 / +
+        String suffixExpression = "4 5 * 8 - 60 + 8 2 / +";
 
         //1. 先将 "3 4 + 5 × 6 - " => 放到ArrayList中
         //2. 将 ArrayList 传递给一个方法，遍历 ArrayList 配合栈 完成计算
+        List<String> list = getListString(suffixExpression);
+        System.out.println(list);//[3, 4, +, 5, ×, 6, -]
+        System.out.println(calculate(list));
     }
 
 
@@ -34,6 +40,49 @@ public class PolandNotation {
             list.add(ele);
         }
         return list;
-
     }
+
+	/**
+     1)从左至右扫描，将3和4压入堆栈；
+     2)遇到+运算符，因此弹出4和3（4为栈顶元素，3为次顶元素），计算出3+4的值，得7，再将7入栈；
+     3)将5入栈；
+     4)接下来是×运算符，因此弹出5和7，计算出7×5=35，将35入栈；
+     5)将6入栈；
+     6)最后是-运算符，计算出35-6的值，即29，由此得出最终结果
+     */
+    //完成对逆波兰表达式的运算
+    public static int calculate(List<String> ls) {
+        // 创建给栈, 只需要一个栈即可
+        Stack<String> stack = new Stack<String>();
+        // 遍历 ls
+        for (String item : ls) {
+            // 这里使用正则表达式来取出数
+            if (item.matches("\\d+")) { // 匹配的是多位数
+                // 入栈
+                stack.push(item);
+            } else {
+                // pop出两个数，并运算， 再入栈
+                int num2 = Integer.parseInt(stack.pop());
+                int num1 = Integer.parseInt(stack.pop());
+                int res = 0;
+                if (item.equals("+")) {
+                    res = num1 + num2;
+                } else if (item.equals("-")) {
+                    res = num1 - num2;
+                } else if (item.equals("*")) {
+                    res = num1 * num2;
+                } else if (item.equals("/")) {
+                    res = num1 / num2;
+                } else {
+                    throw new RuntimeException("运算符有误");
+                }
+                //把res 入栈
+                stack.push("" + res);
+            }
+
+        }
+        //最后留在stack中的数据是运算结果
+        return Integer.parseInt(stack.pop());
+    }
+
 }
