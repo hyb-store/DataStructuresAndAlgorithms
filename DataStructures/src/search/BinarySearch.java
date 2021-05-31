0a18418d0b3e587b8decf6eeb1f15ce744610a50
hyb-store <3063687049@qq.com>
2021-05-31 21:30:58 +0800

    查找

---
+++ DataStructures/src/search/BinarySearch.java
@@ -0,0 +1,79 @@
+package search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 二分查找
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+//        int arr[] = {1, 8, 10, 89, 1000, 1000, 1234};
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11, 12, 13,14,15,16,17,18,19,20 ,20,20,20,21};
+
+        int i = binarySearch(arr, 0, arr.length - 1, 1000);
+        System.out.println(i);
+
+        List<Integer> list = binarySearch2(arr, 0, arr.length - 1, 20);
+        System.out.println(""+list);
+    }
+
+    /**
+     * 二分查找算法
+     *
+     * @param arr     数组
+     * @param left    左边的索引
+     * @param right   右边的索引
+     * @param findVal 要查找的值
+     * @return 如果找到就返回下标，如果没有找到，就返回 -1
+     */
+    public static int binarySearch(int[] arr, int left, int right, int findVal) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = (right + left) / 2;
+        int midVal = arr[mid];
+        if (findVal > midVal) { //右边 递归
+            return binarySearch(arr, mid + 1, right, findVal);
+        } else if (findVal < midVal) {//左边 递归
+            return binarySearch(arr, left, mid - 1, findVal);
+        } else {
+            return mid;
+        }
+    }
+
+    /**
+     * 课后思考题： {1,8, 10, 89, 1000, 1000，1234} 当一个有序数组中，
+     * 有多个相同的数值时，如何将所有的数值都查找到，比如这里的 1000
+     * 1. 在找到mid 索引值，不要马上返回
+     * 2. 向mid 索引值的左边扫描，将所有满足 1000， 的元素的下标，加入到集合ArrayList
+     * 3. 向mid 索引值的右边扫描，将所有满足 1000， 的元素的下标，加入到集合ArrayList
+     * 4. 将Arraylist返回
+     */
+    public static List<Integer> binarySearch2(int[] arr, int left, int right, int findVal) {
+        if (left > right) {
+            return new ArrayList<Integer>();
+        }
+        int mid = (right + left) / 2;
+        int midVal = arr[mid];
+        if (findVal > midVal) { //右边 递归
+            return binarySearch2(arr, mid + 1, right, findVal);
+        } else if (findVal < midVal) {//左边 递归
+            return binarySearch2(arr, left, mid - 1, findVal);
+        } else {
+            List<Integer> indexList = new ArrayList<Integer>();
+            int temp = mid - 1;
+            while(findVal == arr[temp] && temp <= right) {
+                indexList.add(temp);
+                temp--;
+            }
+            indexList.add(mid);
+            temp = mid + 1;
+            while(findVal == arr[temp] && temp >= left) {
+                indexList.add(temp);
+                temp++;
+            }
+            return indexList;
+        }
+    }
+}
