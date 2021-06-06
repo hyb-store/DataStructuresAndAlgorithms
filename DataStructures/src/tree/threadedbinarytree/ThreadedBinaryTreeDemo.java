7961b19cc1f4bacce74739c334d81b2cbd9ce49d
hyb-store <3063687049@qq.com>
2021-06-06 22:45:53 +0800

    .

--- DataStructures/src/tree/threadedbinarytree/ThreadedBinaryTreeDemo.java
+++ DataStructures/src/tree/threadedbinarytree/ThreadedBinaryTreeDemo.java
@@ -1,9 +1,41 @@
 package tree.threadedbinarytree;
 
+/**
+ * 线索化
+ */
 public class ThreadedBinaryTreeDemo {
 
     public static void main(String[] args) {
+        //测试一把中序线索二叉树的功能
+        HeroNode root = new HeroNode(1, "tom");
+        HeroNode node2 = new HeroNode(3, "jack");
+        HeroNode node3 = new HeroNode(6, "smith");
+        HeroNode node4 = new HeroNode(8, "mary");
+        HeroNode node5 = new HeroNode(10, "king");
+        HeroNode node6 = new HeroNode(14, "dim");
 
+        //二叉树，后面我们要递归创建, 现在简单处理使用手动创建
+        root.setLeft(node2);
+        root.setRight(node3);
+        node2.setLeft(node4);
+        node2.setRight(node5);
+        node3.setLeft(node6);
+
+        //测试中序线索化
+        ThreadedBinaryTree threadedBinaryTree = new ThreadedBinaryTree();
+        threadedBinaryTree.setRoot(root);
+        threadedBinaryTree.threadedNodes();
+
+        //测试: 以10号节点测试
+        HeroNode leftNode = node5.getLeft();
+        HeroNode rightNode = node5.getRight();
+        System.out.println("10号结点的前驱结点是 ="  + leftNode); //3
+        System.out.println("10号结点的后继结点是="  + rightNode); //1
+
+        //当线索化二叉树后，能在使用原来的遍历方法
+        //threadedBinaryTree.infixOrder();
+        System.out.println("使用线索化的方式遍历 线索化二叉树");
+        threadedBinaryTree.threadedList(); // 8, 3, 10, 1, 14, 6
     }
 }
 //定义ThreadedBinaryTree 实现了线索化功能的二叉树
@@ -64,17 +96,14 @@ class ThreadedBinaryTree {
         //(一)先线索化左子树
         threadedNodes(node.getLeft());
         //(二)线索化当前结点[有难度]
-
         //处理当前结点的前驱结点
-        //以8结点来理解
-        //8结点的.left = null , 8结点的.leftType = 1
+        //以8结点来理解，8结点的.left = null , 8结点的.leftType = 1
         if(node.getLeft() == null) {
             //让当前结点的左指针指向前驱结点
             node.setLeft(pre);
             //修改当前结点的左指针的类型,指向前驱结点
             node.setLeftType(1);
         }
-
         //处理后继结点
         if (pre != null && pre.getRight() == null) {
             //让前驱结点的右指针指向当前结点
